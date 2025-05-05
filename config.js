2296b3b1e6d5fba15d8088a5a7bd7042066ecc9c
Nutonspeed <nuttapong161@gmail.com>
2025-05-06 02:46:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
-const supabase = window.supabase.createClient(
+const supabase = createClient(
   'https://zhiflvlhamuijgxgtdlf.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoaWZsdmxoYW11aWpneHRnZGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ2NzY1MDUsImV4cCI6MjAyOTI1MjUwNX0.5W_gzDeT2bwL1vdRSU_6X6r_A_dhT1mQdCDmWwbkgVY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoaWZ2bGhtYXVpamd4Z3RkbGYiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNDYxNjUwMiwiZXhwIjoyMDY5OTkyNTAyfQ.nWwaEyhVGkczv2W77cMIXW90NGuXCIKHX90_X3nfhPw'
 );
